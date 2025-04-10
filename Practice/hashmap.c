e1b0190e44e579b872a3709bce06bfaf22e8cdef
apotato369550 <65766356+apotato369550@users.noreply.github.com>
2025-04-10 11:28:19 +0800

    Added insertion through open addressing and separate chaining

--- Practice/hashmap.c
+++ Practice/hashmap.c
@@ -4,12 +4,15 @@
 
 #define HASHMAP_MAX_LENGTH 10
 #define MAX_NAME_LENGTH 50
+#define DELETED_PERSON -2
 
 // steps of creating a hash table
 // 1. create a struct for the data to be stored in the hash table
 // 2. create a hash function to convert the data into an index
-// 3. create a table to store the data (in the form of an array of structs)
-// 4. create a function to insert data into the hash table
+// 3. create a table to store the data (in the form of an array of structs) and a function to initialize it
+// 4. create a function to insert data into the hash table (separate chaining and open addressing)
+// 5. create a function to delete data from the hash table
+// 6. create a function to look up data from the hash table
 
 typedef struct Person {
     char name[MAX_NAME_LENGTH];
@@ -31,7 +34,8 @@ int hash(char *name) {
     return hash;
 }
 
-int insertToHashTable(Person *hashTable, Person person) {
+// separate chaining
+int insertSeparateChaining(Person *hashTable, Person person) {
     printf("Inserting to hash table!");
     int index = hash(person.name);
     
@@ -39,7 +43,7 @@ int insertToHashTable(Person *hashTable, Person person) {
     if (hashTable[index].age != -1) {
         // insert new person at index and stuff
         Person *current = &(hashTable[index]);
-        while (current->next == NULL) {
+        while (current->next != NULL) {
             current = current->next;
         }
         Person *newPerson = malloc(sizeof(Person));
@@ -56,6 +60,42 @@ int insertToHashTable(Person *hashTable, Person person) {
     return 1;
 }
 
+// openAddressing
+int insertOpenAddressing(Person *hashTable, Person person) {
+    int index = hash(person.name);
+    for (int i = 0; i < HASHMAP_MAX_LENGTH; i++) {
+        int currentIndex = (index + i) % HASHMAP_MAX_LENGTH;
+        if (hashTable[currentIndex].age == -1) {
+            strcpy(hashTable[index].name, person.name);
+            hashTable[index].age = person.age;
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int deleteEntry(char *name) {
+    return 1;
+}
+
+Person *findPerson(Person *hashTable, char *name) {
+    // fix this
+    for (int i = 0; i < HASHMAP_MAX_LENGTH; i++) {
+        Person *currentPerson = &(hashTable[i]);
+        if (strcmp(currentPerson->name, name) != 0) {
+            while(currentPerson != NULL) {
+                if (strcmp(currentPerson->name, name) == 0) {
+                    return currentPerson;
+                }
+                currentPerson = currentPerson->next;
+            }
+        } else {
+            return currentPerson;
+        }
+    }
+    return NULL;
+}
+
 void initializeHashTable(Person *hashTable) {
     for (int i = 0; i < HASHMAP_MAX_LENGTH; i++) {
         hashTable[i].age = -1;
@@ -67,6 +107,7 @@ void initializeHashTable(Person *hashTable) {
 void printHashTable(Person *hashTable) {
     printf("Printing Hash Table: \n");
     for (int i = 0; i < HASHMAP_MAX_LENGTH; i++) {
+        printf("Current Index: \n");
         Person *current = &(hashTable[i]);
         while (current != NULL) {
             printf("Name: %s \t---\t Age: %d \n", current->name, current->age);
@@ -92,12 +133,20 @@ int main() {
     printf("Hash Value for %s: %d\n", jessica.name, hash(jessica.name));
     printf("Hash Value for %s: %d\n", jill.name, hash(jill.name));
 
-    insertToHashTable(personHashTable, jake);
-    insertToHashTable(personHashTable, john);
-    insertToHashTable(personHashTable, jeremy);
-    insertToHashTable(personHashTable, jessica);
-    insertToHashTable(personHashTable, jill);
-    
+    insertSeparateChaining(personHashTable, jake);
+    insertSeparateChaining(personHashTable, john);
+    insertSeparateChaining(personHashTable, jeremy);
+    insertSeparateChaining(personHashTable, jessica);
+    insertSeparateChaining(personHashTable, jill);
+
     printHashTable(personHashTable);
+    
+    Person *foundPerson = findPerson(personHashTable, "John");
+    if (foundPerson) {
+        printf("Person found!: %s Age: %d\n", foundPerson->name, foundPerson->age);
+    } else {
+        printf("Person not found. \n");
+    }
+    
     return 0;
 }
