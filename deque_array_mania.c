800d27b07e2a20d17408255e9d7ebc8f1ebfb5e5
John Andre Yap <johnandresyap510@gmail.com>
2025-04-26 11:51:56 +0800

    Added some functionality to functions (to be tested)

--- deque_array_mania.c
+++ deque_array_mania.c
@@ -4,7 +4,7 @@
 
 // declare different constants
 #define DEQUE_EMPTY INT_MIN
-#define DEQUE_FULL INT_MAX
+#define EMPTY INT_MAX
 #define MAX_DEQUE_SIZE 10
 
 // array implementation of a deque
@@ -31,10 +31,10 @@ void displayDeque(Deque deque);
 void put(Deque *deque, int value);
 
 // deqeue - removes and returns the element at the front of the array
-void dequeue(Deque *deque);
+int dequeue(Deque *deque);
 
 // pop - removes and returns the element at the tail of the array
-void pop(Deque *deque);
+int pop(Deque *deque);
 
 // isEmpty - checks if deque is empty
 int isEmpty(Deque *deque);
@@ -67,15 +67,28 @@ int main() {
                 break;
             case 1:
                 printf("Displaying all items in deque...\n");
+                displayDeque(deque);
                 break;
             case 2:
                 printf("Putting an item into the deque... \n");
+                int value = 0;
+                printf("Please enter value to put: ");
+                scanf("%d", &value);
+                put(&deque, value);
                 break;
             case 3:
                 printf("Dequeueing an item from the deque... \n");
+                int dequeued = dequeue(&deque);
+                if (dequeued != DEQUE_EMPTY) {
+                    printf("Dequeued value: %d\n", dequeued);
+                }
                 break;
             case 4:
                 printf("Popping an item from the deque...\n");
+                int popped = dequeue(&deque);
+                if (popped != DEQUE_EMPTY) {
+                    printf("Popped value: %d\n", popped);
+                }
                 break;
             default:
                 printf("Please enter a valid option...\n");
@@ -95,9 +108,14 @@ void initializeDeque(Deque *deque) {
 
 // display - display the elements of the deqeue
 void displayDeque(Deque deque) {
+    // fix this
+    // DEFINITELY fix this
+    printf("---- START OF DEQUE ---\n");
     for (int i = 0; i < deque.count; i++) {
-        printf("Index [%d]: %d\n", i, deque.array[i]);
+        int try = (deque.tail + i) % MAX_DEQUE_SIZE;
+        printf("Index [%d]: %d\n", i, deque.array[try]);
     }
+    printf("---- END  OF DEQUE ---\n");
 }
 
 // push/enqueue/put function - adds element to the end of the array
@@ -107,18 +125,43 @@ void put(Deque *deque, int value) {
         return;
     }
     deque->array[deque->tail] = value;
-    deque->tail += (deque->tail + 1) % MAX_DEQUE_SIZE;
+    deque->tail = (deque->tail + 1) % MAX_DEQUE_SIZE;
+    printf("Value of tail: %d\n", deque->tail);
     deque->count += 1;
+    printf("Successfully put value (%d) in deque.\n", value);
 }
 
-// deqeue - removes and returns the element at the front of the array
-void dequeue(Deque *deque);
+// dequeue - removes and returns the element at the front of the array
+int dequeue(Deque *deque) {
+    if (isEmpty(deque)) {
+        printf("Deque is empty! Failed to dequeue...\n");
+        return DEQUE_EMPTY;
+    }
+    int dequeuedValue = deque->array[deque->head];
+    deque->head = (deque->head + 1) % MAX_DEQUE_SIZE;
+    deque->count -= 1;
+    return dequeuedValue;
+}
 
 // pop - removes and returns the element at the tail of the array
-void pop(Deque *deque);
+int pop(Deque *deque) {
+    // there's something wrong with pop
+    if (isEmpty(deque)) {
+        printf("Deque is empty! Failed to dequeue...\n");
+        return DEQUE_EMPTY;
+    }
+    int popped = deque->array[deque->tail];
+    deque->tail = (deque->tail - 1) % MAX_DEQUE_SIZE;
+    deque->count -= 1;
+    return popped;
+}
 
 // isEmpty - checks if deque is empty
-int isEmpty(Deque *deque);
+int isEmpty(Deque *deque) {
+    return deque->count <= 0 ? 1 : 0;
+}
 
 // isfull - checks if deque is full
-int isFull(Deque *deque);
+int isFull(Deque *deque) {
+    return deque->count >= MAX_DEQUE_SIZE - 1 ? 1 : 0;
+}
