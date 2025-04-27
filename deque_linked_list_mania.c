399dc1f2001822f1c5f980515291494d8cbbce8a
John Andre Yap <johnandresyap510@gmail.com>
2025-04-27 09:21:27 +0800

    Started with linked list implementation of deques

---
+++ deque_linked_list_mania.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// array implementation of a deque
+// todo:
+// node struct - basic node struct for lls
+
+
+
+// dequeue struct - stores head, tail of deque
+
+// selection menu - user selects stuff
+
+// function prototypes
+// initialize - sets head and tail to null
+// display - display all values in deque
+// put - places a node/inserts an item at the end of the deque
+// deque - removes and returns the item at the head of the deque
+// pop - removes an item from the tail of the queue
+
+// things to look out for:
+// when head and tail overlap
+// (check other code for details)
+
+int main () {
+
+    return 0;
+}
