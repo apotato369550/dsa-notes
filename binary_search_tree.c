fb98bb0f5e762d02b1a47130584f684f7a6157c5
John Andre Yap <johnandresyap510@gmail.com>
2025-05-03 13:28:48 +0800

    Began with basic binary tree template

--- binary_search_tree.c
+++ binary_search_tree.c
@@ -1,7 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct TreeNode {
+    int value;
+    struct TreeNode *left;
+    struct TreeNode *right;
+} TreeNode;
+
+TreeNode *createTreeNode(int value);
+void printIndentation(int indents);
+void printTree(TreeNode *root, int indents);
+
 int main() {
 
     return 0;
+}
+
+TreeNode *createTreeNode(int value) {
+    TreeNode *newNode = malloc(sizeof(TreeNode));
+    if (newNode) {
+        newNode->value = value;
+        newNode->left = NULL;
+        newNode->right = NULL;
+    }
+
+    return newNode;
+}
+
+void printIndentation(int value) {
+    for (int i = 0; i < value; i++) {
+        printf("\t");
+    }
+}
+
+void printTree(TreeNode *root, int indents) {
+    if (root == NULL) {
+        printIndentation(indents);
+        printf("--- EMPTY ---\n");
+    } else {
+        printTree(root->left, indents + 1);
+        printIndentation(indents);
+        printf("%d \n", root->value);
+        printTree(root->right, indents + 1);
+    }
 }
