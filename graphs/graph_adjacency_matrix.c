c4355cfaa65afce791dbaaa3ce37b6c1abb76b16
apotato369550 <65766356+apotato369550@users.noreply.github.com>
2025-05-26 14:52:26 +0800

    Added hasEdge, addEdge, and removeEdge functions

--- graphs/graph_adjacency_matrix.c
+++ graphs/graph_adjacency_matrix.c
@@ -7,7 +7,10 @@ typedef struct Graph {
 
 GraphPointer createGraph(int n);
 void destroyGraph(GraphPointer graph);
-void printGraph(Graph graph);
+void printGraph(GraphPointer graph);
+int hasEdge(GraphPointer graph, int from, int to);
+int addEdge(GraphPointer graph, int from, int to, int weight);
+int removeEdge(GraphPointer graph, int from, int to);
 
 int main() {
 
@@ -56,13 +59,39 @@ void destroyGraph(GraphPointer graph) {
 
 // change i, j to from, to
 // change graph to *graph, since it'll be dynamically allocated
-void printGraph(Graph graph) {
+void printGraph(GraphPointer graph) {
     printf("----- START OF GRAPH -----\n");
-    for (int i = 0; i < graph.n; i++) {
-        for (int j = 0; j < graph.n; j++) {
-            printf("%d ", graph.edges[i][j]);
+    for (int from = 0; from < graph->n; from++) {
+        for (int to = 0; to < graph->n; to++) {
+            printf("%d ", graph->edges[from][to]);
         }
         printf("\n");
     }
-    printf("----- END OF GRAPH -----\n")
+    printf("----- END OF GRAPH -----\n");
+}
+
+int hasEdge(GraphPointer graph, int from, int to) {
+    return graph->edges[from][to];
+}
+
+int addEdge(GraphPointer graph, int from, int to, int weight) {
+    if (hasEdge(graph, from, to)) {
+        return 0;
+    }
+
+    graph->edges[from][to] = weight;
+
+    return 1;
+}
+
+
+
+int removeEdge(GraphPointer graph, int from, int to) {
+    if (!hasEdge(graph, from, to)) {
+        return 0;
+    }
+
+    graph->edges[from][to] = 0;
+
+    return 1;
 }
