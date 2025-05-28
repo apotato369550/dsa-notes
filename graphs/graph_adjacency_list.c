5af8e33d6de605fd26aca9dcebc85be714926bb3
John Andre Yap <johnandresyap510@gmail.com>
2025-05-28 13:08:30 +0800

    Got started with adjacency list implementation of graphs

--- graphs/graph_adjacency_list.c
+++ graphs/graph_adjacency_list.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// declare structs for graph, and graphnode
+
+typedef struct GraphNode {
+    char vertex;
+    int weight;
+    struct GraphNode *next;
+} GraphNode, *GraphNodePointer;
+
+typedef struct Graph {
+    GraphNodePointer adjacency_list;
+} Graph, *GraphPointer;
+
+GraphPointer createGraph(GraphPointer adjacency_list, int n);
+int hasEdge(GraphPointer adjacency_list, char from, char to);
+int addEdge(GraphPointer adjacency_list, char from, char to);
+int removeEdge(GraphPointer adjacency_list, char from, char to);
+void printGraph(GraphPointer adjacency_list);
+int destroyGraph(GraphPointer adjacency_list);
+
 int main() {
+    
+
     return 0;
 }
