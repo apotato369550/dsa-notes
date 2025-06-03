0645f63bee13c6717b2298013e064b2898af2507
John Andre Yap <johnandresyap510@gmail.com>
2025-06-03 12:50:11 +0800

    Got started creating function prototypes for pointer-based minheap

--- heaps/heap_tree.c
+++ heaps/heap_tree.c
@@ -3,6 +3,26 @@
 
 // figure out how to implement a minheap using tree/pointers
 
+typedef struct HeapNode {
+    int value;
+    struct HeapNode *left;
+    struct HeapNode *right;
+    struct HeapNode *parent;
+} HeapNode, *Root;
+
+HeapNode *createNode(int value);
+void printTabs(int tabs);
+void printTree(Root root, int tabs);
+HeapNode *findInsertionPoint(Root *root);
+void insertNode(Root *root, int value);
+int extractMinimum(Root *root);
+int peekMinimum(Root *root);
+void heapifyUp(Root *root);
+void heapifyDown(Root *root);
+int getNodeCount(Root *root);
+
+// continue making function prototypes
+
 int main() {
     return 0;
 }
