84e3c87b510018734d7cee8a5d09d45db991fb32
John Andre Yap <johnandresyap510@gmail.com>
2025-06-04 12:43:54 +0800

    Finished with function prototypes and templates

--- heaps/heap_tree.c
+++ heaps/heap_tree.c
@@ -20,9 +20,57 @@ int peekMinimum(Root *root);
 void heapifyUp(Root *root);
 void heapifyDown(Root *root);
 int getNodeCount(Root *root);
+void destroyHeap(Root *root);
 
 // continue making function prototypes
 
 int main() {
     return 0;
+}
+
+HeapNode *createNode(int value) {
+    return NULL;
+}
+
+void printTabs(int tabs) {
+    return;
+}
+
+void printTree(Root root, int tabs) {
+    return;
+}
+
+HeapNode *findInsertionPoint(Root *root) {
+    return NULL;
+}
+
+void insertNode(Root *root, int value) {
+    return;
+}
+
+int extractMinimum(Root *root) {
+    int minimum = (*root)->value;
+    // free root, heapify down or smth
+    return minimum;
+}
+
+int peekMinimum(Root *root) {
+    return (*root)->value;
+}
+
+void heapifyUp(Root *root) {
+    return;
+}
+
+void heapifyDown(Root *root) {
+    return; 
+}
+
+int getNodeCount(Root *root) {
+    // something something recursive count
+    return 0;
+}
+
+void destroyHeap(Root *root) {
+    return;
 }
