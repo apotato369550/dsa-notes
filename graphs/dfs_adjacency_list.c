46deb1328f498c7157820ed6fe0624f923e62c09
John Andre Yap <johnandresyap510@gmail.com>
2025-06-27 09:26:40 +0800

    Added stuff I forgot to push

--- graphs/dfs_adjacency_list.c
+++ graphs/dfs_adjacency_list.c
@@ -1,7 +1,158 @@
 #include <stdio.h>
 
 
+typedef struct GraphNode {
+    char vertex;
+    int weight;
+    struct GraphNode *next;
+} GraphNode, *GraphNodePointer;
+
+typedef struct Graph {
+    int n;
+    GraphNodePointer adjacency_list;
+} Graph, *GraphPointer;
+
+GraphPointer createGraph(int n);
+int addEdge(GraphPointer graph, char from, char to, int weight);
+void printGraph(GraphPointer graph);
+int destroyGraph(GraphPointer graph);
+
+void DFS_explore(GraphPointer graph, char start, int *visited);
+void DFS_target(GraphPointer graph, char start, char target);
 
 int main() {
+    Graph *graph = createGraph(10);
+
+    if (graph == NULL) {
+        printf("Failed to allocate memory...\n");
+        return 1;
+    }
+    addEdge(graph, 'A', 'B', 1); 
+    addEdge(graph, 'B', 'A', 1);
+
+    addEdge(graph, 'A', 'C', 1); 
+    addEdge(graph, 'C', 'A', 1);
+
+    addEdge(graph, 'B', 'D', 1); 
+    addEdge(graph, 'D', 'B', 1);
+
+    addEdge(graph, 'C', 'E', 1); 
+    addEdge(graph, 'E', 'C', 1);
+
+    addEdge(graph, 'D', 'F', 1); 
+    addEdge(graph, 'F', 'D', 1);
+
+    addEdge(graph, 'E', 'G', 1); 
+    addEdge(graph, 'G', 'E', 1);
+
+    addEdge(graph, 'F', 'G', 1);
+    addEdge(graph, 'G', 'F', 1);
+
+    addEdge(graph, 'G', 'H', 1); 
+    addEdge(graph, 'H', 'G', 1);
+
+    addEdge(graph, 'I', 'J', 1);
+    addEdge(graph, 'J', 'I', 1);
+
+    int *visited = calloc(graph->n, sizeof(int));
+
+    DFS_explore(graph, 'A', visited);
+
+    DFS_target(graph->adjacency_list, 'A', 'H');   // should find path
+    DFS_target(graph->adjacency_list, 'A', 'I');   // should report not reachable
+
+    destroyGraph(graph);
+
     return 0;
+}
+
+GraphPointer createGraph(int n) {
+    Graph *graph = malloc(sizeof(Graph));
+
+    if (graph == NULL) {
+        return NULL;
+    }
+
+    graph->n = n;
+    graph->adjacency_list = malloc(sizeof(GraphNode) * graph->n);
+
+    if (graph->adjacency_list == NULL) {
+        return NULL;
+    }
+
+    for (int i = 0; i < n; i++) {
+        graph->adjacency_list[i].vertex = (char) ((int) 'A' + i);
+        graph->adjacency_list[i].weight = 0;
+        graph->adjacency_list[i].next = NULL;
+    }   
+
+    return graph;
+}
+
+
+int addEdge(GraphPointer graph, char from, char to, int weight) {
+    int fromVertex = hasVertex(graph, from);
+    int toVertex = hasVertex(graph, to);
+    if (fromVertex == -1 || toVertex == -1) {
+        printf("Vertex %c or %c does not exist...\n", from, to);
+        return -1;
+    }
+
+    if (hasEdge(graph, from, to) >= 0) {
+        printf("An edge already exists from %c to %c...\n", from, to);
+        return -1;
+    }
+
+    // insert last becoz ez
+    // this should works :V
+    GraphNode *newGraphNode = malloc(sizeof(GraphNode));
+    newGraphNode->vertex = to;
+    newGraphNode->next = NULL;
+    newGraphNode->weight = weight;
+    GraphNodePointer current = &(graph->adjacency_list[fromVertex]);
+    while (current->next != NULL) {
+        current = current->next;
+    }
+    current->next = newGraphNode;
+
+    return 1;
+}
+
+void printGraph(GraphPointer graph) {
+    printf("----- START OF GRAPH -----\n");
+    for (int i = 0; i < graph->n; i++) {
+        printf("Vertex %c: ", graph->adjacency_list[i].vertex);
+
+        GraphNodePointer current = graph->adjacency_list[i].next;
+        while (current != NULL) {
+            printf("%c (%d)", current->vertex, current->weight);
+            if (current->next != NULL) {
+                printf(" -> ");
+            }
+            current = current->next;
+        }
+
+        printf("\n");
+    }
+    printf("----- END OF GRAPH -----\n");
+    return;
+}
+
+int destroyGraph(GraphPointer graph) {
+    free(graph->adjacency_list);
+    free(graph);
+}
+
+void DFS_explore(GraphPointer graph, char start, int *visited) {
+    int is_visited = 0;
+    for (int i = 0; i < graph->n; i++) {
+        if (visited[(int) start - (int) 'A'] == 1) {
+            is_visited = 1;
+            break;
+        }
+    }
+}
+
+void DFS_target(GraphPointer graph, char start, char target) {
+    return;
 }
